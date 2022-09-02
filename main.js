7d19eab859e3497c73c69ce47f3fe4ae84affddf
hama-khfifi <hama@gmail.com>
2022-09-02 10:25:51 +0100

    ws dom

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ console.log(buttonsMinus)
 
 for (let btnPlus of buttonsMinus){
     btnPlus.addEventListener('click' ,function(){
-        btnPlus.previousElementSibling.innerHTML--
+        btnPlus.previousElementSibling.innerHTML-- 
         sum()
     })
 } 
@@ -25,15 +25,30 @@ function sum(){
     for(let i = 0 ; i <qte.length ; i++)
     sum +=qte[i].innerHTML * price[i].innerHTML
 }
-document.getElementById('total').innerHTML ="Shopping Bag Total : $" +sum
+// document.getElementById('total').innerHTML ="Shopping Bag Total : $" +sum
 
 
 let buttonsDelete = document.getElementsByClassName('fa-trash')
 
-for (let buttonsDelete of buttonsDelete){
-    buttonsDelete.addEventListener('click' ,function(){
-        buttonsDelete.parentNode.remove();
+for (let X of buttonsDelete){
+    X.addEventListener('click' ,function(){
+        X.parentNode.remove();
         sum()
     })
 
+}
+
+let thumbSup = document.querySelectorAll('.fa-thumbs-up');
+for(let thumbSup1 of thumbSup){
+    thumbSup1.addEventListener('click' ,function(){
+        thumbSup1.classList.toggle('red')
+    })
+}
+
+let bottonBuy= document.getElementsByClassName('btn3');
+
+for(let btnBuy of bottonBuy){
+    btnBuy.addEventListener('click' ,function(){
+        alert("we will call you soon")
+    })
 }
